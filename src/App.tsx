a61612c1e1122c296fd08734521ecf87e972d0a5
hachibu <raymondsohn@gmail.com>
2022-09-23 15:58:05 -0700

    Switch to no-store cache

--- src/App.tsx
+++ src/App.tsx
@@ -55,10 +55,11 @@ function App() {
 
     for (let i = 0; i < runs; i++) {
       const startTimeMs = performance.now()
-      const response = await fetch(fetchUrl, { mode: "no-cors", cache: "no-cache", })
+      const response = await fetch(fetchUrl, { cache: "no-store" })
       if (!response.ok) {
         alert(`Error fetching ${fetchUrl}`)
-        break
+        console.error(response)
+        return
       }
       const stopTimeMs = performance.now()
       const timeMs = stopTimeMs - startTimeMs
