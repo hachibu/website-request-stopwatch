3163d05169ee6760a9384f6c67a1fb496a496da1
hachibu <raymondsohn@gmail.com>
2022-09-24 14:24:26 -0700

    Add more options for sample size

--- src/App.tsx
+++ src/App.tsx
@@ -119,7 +119,9 @@ function App() {
             value={sampleSize}
           >
             <option value="10">10</option>
+            <option value="50">50</option>
             <option value="100">100</option>
+            <option value="500">500</option>
             <option value="1000">1000</option>
           </select>
         </div>
