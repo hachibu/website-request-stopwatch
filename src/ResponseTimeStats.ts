b03314f66ff07f9429bf785e13fac6a00d30bccc
hachibu <raymondsohn@gmail.com>
2022-10-02 11:03:50 -0700

    Calculate percentiles in a loop

--- src/ResponseTimeStats.ts
+++ src/ResponseTimeStats.ts
@@ -13,21 +13,24 @@ export interface ResponseTimeStats {
 export function calculateResponseTimeStats(
   data: number[] = []
 ): ResponseTimeStats {
-  const callOrDefaultZero = (f: Function, data: number[], ...args: any[]) =>
-    data.length > 1 ? f(data, ...args) : 0;
-
-  return {
+  const stats: ResponseTimeStats = {
     size: data.length,
     mean: callOrDefaultZero(ss.mean, data),
     median: callOrDefaultZero(ss.median, data),
     stdev: callOrDefaultZero(ss.sampleStandardDeviation, data),
     min: callOrDefaultZero(ss.min, data),
     max: callOrDefaultZero(ss.max, data),
-    percentiles: {
-      0.75: callOrDefaultZero(ss.quantile, data, 0.75),
-      0.9: callOrDefaultZero(ss.quantile, data, 0.9),
-      0.95: callOrDefaultZero(ss.quantile, data, 0.95),
-      0.99: callOrDefaultZero(ss.quantile, data, 0.99),
-    },
+    percentiles: {},
   };
+  const percentiles = [0.75, 0.9, 0.95, 0.99];
+
+  for (let p of percentiles) {
+    stats.percentiles[p] = callOrDefaultZero(ss.quantile, data, p);
+  }
+
+  return stats;
+}
+
+function callOrDefaultZero(f: Function, data: number[], ...args: any[]) {
+  return data.length > 1 ? f(data, ...args) : 0;
 }
